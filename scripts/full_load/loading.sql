a792fe18d7db1c619b6c7e3f9b3c73b724aeae93
Immaculate Okoro <ermmaculate97@gmail.com>
2025-10-20 12:07:54 +0100

    Update loading.sql

--- scripts/full_load/loading.sql
+++ scripts/full_load/loading.sql
@@ -1,1 +1,17 @@
 
+  --------full load strategy for all NYC Taxi data
+/*
+This method loads all 2024 yellow_nyc_taxi parquet files from the named stage 
+to table already created at once using the copy into command 
+
+note: All  ETL processes commands and codes were written using  Snowflake warehouse
+*/
+
+--loading data from stage 
+
+    COPY INTO yellow_tripdate_2024
+    FROM @full_load_dec_taxi
+    FILE_FORMAT = parquet
+    MATCH_BY_COLUMN_NAME = CASE_INSENSITIVE;
+
+SELECT * FROM yellow_tripdate_2024
