0a71881314a155e574f8e42712477f1cb6817c43
Immaculate Okoro <ermmaculate97@gmail.com>
2025-10-20 12:03:13 +0100

    Update ddl.sql

--- scripts/full_load/ddl.sql
+++ scripts/full_load/ddl.sql
@@ -1,1 +1,48 @@
+--to create  database and schema in snowflake
+
+CREATE DATABASE full_load_NYC_TAXI;
+
+CREATE SCHEMA bronze;
+
+CREATE SCHEMA silver;
+
+CREATE SCHEMA gold;
+
+USE SCHEMA bronze;
+
+
+
+--create file  format
+CREATE OR REPLACE FILE FORMAT parquet
+    TYPE = PARQUET;
+
+-- Create an internal stage.
+CREATE OR REPLACE STAGE full_load_dec_taxi
+FILE_FORMAT = parquet;
+
+--to access files in stage
+list '@full_load_dec_taxi'
+
+--- Query the INFER_SCHEMA function.
+ SELECT *
+  FROM TABLE(
+    INFER_SCHEMA(
+      LOCATION=>'@full_load_dec_taxi'
+      , FILE_FORMAT=>'parquet'
+      )
+    );
+
+---create table with the inferred schema 
+    CREATE OR REPLACE TABLE yellow_tripdate_2024
+USING TEMPLATE (
+  SELECT ARRAY_AGG(OBJECT_CONSTRUCT(*))
+  FROM TABLE(
+    INFER_SCHEMA(
+      LOCATION => '@full_load_dec_taxi', 
+      FILE_FORMAT => 'parquet'
+    )
+  )
+      
+  SELECT * FROM yellow_tripdate_2024
+
 
