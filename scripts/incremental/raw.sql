1cf5e7b50dfc3e3f69245588eff546f252288581
Immaculate Okoro <ermmaculate97@gmail.com>
2025-10-21 17:37:37 +0100

    Update raw.sql

--- scripts/incremental/raw.sql
+++ scripts/incremental/raw.sql
@@ -1,1 +1,12 @@
+  select * from raw_stream_2024  
+    
 
+
+select * from raw_inc_yellow_tripdate_2024
+
+copy into raw_inc_yellow_tripdate_2024
+from @inc_load_dec_taxi/yellow_tripdata_2024-01.parquet
+ FILE_FORMAT = parquett
+ MATCH_BY_COLUMN_NAME = CASE_INSENSITIVE;
+
+ select * from raw_inc_yellow_tripdate_2024
