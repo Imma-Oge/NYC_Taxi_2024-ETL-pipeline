e168fbc7a717a08e942e8c627c0f828ef6f05c01
Immaculate Okoro <ermmaculate97@gmail.com>
2025-10-30 16:20:29 +0100

    Update ddl.sql

--- scripts/incremental/ddl.sql
+++ scripts/incremental/ddl.sql
@@ -64,3 +64,33 @@ CREATE OR REPLACE TABLE bronze.raw_nyc_taxi
 ---creating stream to track changes(insert) in the raw table
 
 CREATE OR REPLACE STREAM raw_stream ON TABLE bronze.raw_nyc_taxi
+
+
+ -------creating the transformed Table in the silver layer
+
+ CREATE OR REPLACE TABLE silver.trans_nyc_taxi
+(  
+
+    vendor_id NUMBER(38,0),
+    pickup_time timestamp,
+	dropoff_time timestamp,
+    months date,
+    total_passengers NUMBER(38,0),
+    trip_distance FLOAT,
+    distance_group VARCHAR(50),
+    ratecode_group VARCHAR(50),
+    store_and_fwd_indication VARCHAR(50),
+    taximeter_engaged_zone NUMBER(38,0),
+    taximeter_disengaged_zone NUMBER(38,0),
+    payment_type VARCHAR(50),
+    fare_amount FLOAT,
+    extra FLOAT,
+    metered_rate_tax FLOAT,
+    creditcard_tip_amount FLOAT,
+    tolls_amount FLOAT,
+	improvement_surcharge FLOAT, 
+	total_amount_excluding_cash_tip FLOAT,
+	congestion_surcharge FLOAT,
+	LaGuardia_JFK_fee FLOAT,
+    last_updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP() 
+    	
